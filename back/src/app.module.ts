c81d937f64852afa13ec4871d4299e57329f7256
qjetshk <johndoe@example.com>
2026-04-24 16:09:14 +0500

    add auth service & is-dev, convert-time util

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
